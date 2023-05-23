a3dd313ad07567a65d6fa4a426ae8b522471cf99
DGuziur <43893166+DGuziur@users.noreply.github.com>
2023-05-23 15:25:03 +0200

    Update GrafikV3.py

--- GrafikV3.py
+++ GrafikV3.py
@@ -9,7 +9,8 @@ population = []
 work_shifts = ["pn-1","pn-2","pn-3","wt-1","wt-2","wt-3","sr-1","sr-2",
                "sr-3","czw-1","czw-2","czw-3","pt-1","pt-2","pt-3"]
 
-df1 = pd.DataFrame(columns=["osobnik","fitness"])
+df1 = pd.DataFrame(columns=["pn-1","pn-2","pn-3","wt-1","wt-2","wt-3","sr-1","sr-2",
+                            "sr-3","czw-1","czw-2","czw-3","pt-1","pt-2","pt-3","fitness"])
 
 def create_starting_population(i):
     for x in range(i):
@@ -82,5 +83,13 @@ create_starting_population(number_of_parents)
 for i in range(number_of_iterations):
     tournament_selection(population)
     kill_the_weak(population)
-
+    for el in population:
+        df2 = pd.DataFrame([[el[0],el[1],el[2],el[3],el[4],el[5],el[6],el[7],el[8],el[9],
+                             el[10],el[11],el[12],el[13],el[14],evaluate_fitness(el)]],
+                   index=[i + 1],
+                   columns=["pn-1","pn-2","pn-3","wt-1","wt-2","wt-3","sr-1","sr-2",
+                            "sr-3","czw-1","czw-2","czw-3","pt-1","pt-2","pt-3","fitness"])
+        df1 = pd.concat([df1, df2])
+        
+df1.to_excel("Wynik.xlsx")
 print(f"Wynik: {min(population, key=evaluate_fitness)}")
